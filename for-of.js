df8849f931ee209afd6c62fa43aafe2d5052dc97
neshadx <neshadimagify@gmail.com>
2025-02-16 10:21:03 +0600

    added more code

---
+++ for-of.js
@@ -0,0 +1,19 @@
+// let numbers = [1, 2, 3, 4, 5, 5, 7];
+// console.log(numbers);
+
+// for(let num of numbers) {
+//     console.log(num);
+// }
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 10) {
+//     console.log(num);
+//     sum = sum + ngit addum;
+//     console.log("sum:", sum)
+//     num++;
+// }
+
+for (let i = 20; i <= 0; i--) {
+    console.log(i);
+}
